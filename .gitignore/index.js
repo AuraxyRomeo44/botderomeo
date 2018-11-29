0f6990f34bb517c66e1d55c06d0781727e1350d4
AuraxyRomeo44 <45470813+AuraxyRomeo44@users.noreply.github.com>
2018-11-29 23:07:58 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 var prefix = "*";
 
-client.login('NTE3NDU1NzY5NTMzMTUzMjgw.DuGptw.pS2hGw53h7aaJ5TjQt74-9_2Tjg');
+client.login('process.env.TOKEN');
 
 client.on('message' , message =>{
     if(message.content === "salut"){
